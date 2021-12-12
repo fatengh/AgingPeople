11919cafa4b46d56918f6790fd97ab22d7ea75c2
fatengh <faten.arg@hotmail.com>
2021-12-12 17:39:59 +0000

    first commit

--- AgingPeople/ViewController.swift
+++ AgingPeople/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+    let names = ["Fadia" , "Faten" , "Rawya" , "Ruba" , "Manar"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
+
+extension ViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PersonCell", for: indexPath)
+        cell.textLabel?.text = self.names[indexPath.row]
+        
+        
+    let age = Int.random(in: 5...95)
+        cell.detailTextLabel?.text = "\(age)"
+        return cell
+    }
+}
